362f5ae0060c486d95372ec8b90171a91b7cceca
Mulia Sujiastuti Wahyono <muliasujiastuti@gmail.com>
2023-07-21 14:57:39 +0700

    Car details modal

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -72,7 +72,7 @@ const CarCard = ({ car }: CarCardProps ) => {
         </div>
       </div>
 
-      {/* <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car}/> */}
+      <CarDetails isOpen={isOpen} closeModal={() => setIsOpen(false)} car={car}/>
     </div>
   )
 }
