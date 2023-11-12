32f1607a39d5afeb0ebd36117dc41c24d6b4b479
Mulia Sujiastuti Wahyono <muliasujiastuti@gmail.com>
2023-11-12 16:41:51 +0700

    add custom filter

---
+++ components/ShowMore.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useRouter } from 'next/navigation';
+import React from 'react'
+
+const ShowMore = ({ pageNumber, isNext }) => {
+  return (
+    <div>ShowMore</div>
+  )
+}
+
+export default ShowMore
--- types/index.ts
+++ types/index.ts
@@ -30,6 +30,24 @@ export interface CarProps {
     year: number;
 }
 
+export interface FilterProps {
+    manufacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
+}
+
+export interface OptionProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+}
+
 export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
     const mileageFactor = 0.1; // Additional rate per mile driven
